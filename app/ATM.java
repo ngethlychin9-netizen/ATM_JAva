b68c01456c865b0dbba0ba5d414c1cdd59b69b58
lychin <ngethlychin@gmail.com>
2026-09-22 23:25:13 +0700

    ATM v1.1 just update

--- app/ATM.java
+++ app/ATM.java
@@ -4,30 +4,38 @@ import java.util.Scanner;
 
 public class ATM {
 
-    Scanner sc = new Scanner(System.in);
+    private Scanner scanner;
+
+    public ATM() {
+        scanner = new Scanner(System.in);
+    }
 
     public void start() {
 
-        while(true) {
+        while (true) {
 
             System.out.println("\n===== ATM SYSTEM =====");
             System.out.println("1. Login");
             System.out.println("2. Exit");
+            System.out.print("Choose: ");
 
-            int choice = sc.nextInt();
+            int choice = scanner.nextInt();
 
-            switch(choice) {
+            switch (choice) {
 
                 case 1:
-                    System.out.println("Login Function");
+                    System.out.println(
+                            "Login feature coming soon...");
                     break;
 
                 case 2:
-                    System.out.println("Good Bye");
+                    System.out.println(
+                            "Thank you for using ATM.");
                     return;
 
                 default:
-                    System.out.println("Invalid Choice");
+                    System.out.println(
+                            "Invalid choice.");
             }
         }
     }
