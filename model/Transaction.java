042f4703178cff36e1fb3b5921f05b1bf096b984
lychin <ngethlychin@gmail.com>
2026-09-22 23:09:21 +0700

    transaction.java

---
+++ model/Transaction.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Transaction {
+
+    private String type;
+    private double amount;
+    private double balanceAfter;
+    private LocalDateTime dateTime;
+
+    public Transaction(String type,
+                       double amount,
+                       double balanceAfter) {
+
+        this.type = type;
+        this.amount = amount;
+        this.balanceAfter = balanceAfter;
+        this.dateTime = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return type + " | " +
+               amount + " | " +
+               dateTime + " | " +
+               balanceAfter;
+    }
+}
