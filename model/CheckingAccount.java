247492abebd8ad9b45b389211923bbd551111d2d
Maly pung <malylyyuya@gmail.com>
2026-09-22 22:41:23 +0700

    Checking Account is added with 3 letters error exception

---
+++ model/CheckingAccount.java
@@ -0,0 +1,35 @@
+package model;
+
+public class CheckingAccount {
+    
+}package model;
+
+import exception.InsufficientFundsException;
+
+public class CheckingAccount extends Account {
+
+    private final double DAILY_LIMIT = 1000;
+
+    public CheckingAccount(String accountNumber,
+                           String pin,
+                           double balance) {
+        super(accountNumber, pin, balance);
+    }
+
+    @Override
+    public void withdraw(double amount)
+            throws InsufficientFundsException {
+
+        if(amount > DAILY_LIMIT) {
+            throw new InsufficientFundsException(
+                    "Daily limit exceeded");
+        }
+
+        if(amount > getBalance()) {
+            throw new InsufficientFundsException(
+                    "Insufficient Balance");
+        }
+
+        deductBalance(amount);
+    }
+}
