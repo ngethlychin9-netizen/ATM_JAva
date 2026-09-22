0b095ffb48cd66722fae462372dd8d0d2e3cece7
lychin <ngethlychin@gmail.com>
2026-09-22 23:43:24 +0700

    authentication Service v1.1 is uploaded

--- service/AuthenticationService.java
+++ service/AuthenticationService.java
@@ -4,14 +4,31 @@ import model.Account;
 
 public class AuthenticationService {
 
-    public boolean login(Account account,
-                         String enteredPin) {
+    public boolean login(Account account, String enteredPin) {
 
         if (account == null) {
             return false;
         }
 
-        return account.getPin().equals(enteredPin);
+        if (account.isLocked()) {
+            System.out.println("Account is locked.");
+            return false;
+        }
+
+        if (account.getPin().equals(enteredPin)) {
+            account.setFailedAttempts(0);
+            return true;
+        }
+
+        account.setFailedAttempts(
+                account.getFailedAttempts() + 1);
+
+        if (account.getFailedAttempts() >= 3) {
+            account.lockAccount();
+            System.out.println("Account locked!");
+        }
+
+        return false;
     }
 
     public boolean changePin(Account account,
