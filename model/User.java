ece8d75d36ad1b8a6393d8a856f9c88266f1682d
lychin <ngethlychin@gmail.com>
2026-09-22 22:56:40 +0700

    user file is added for user fill infor mation

--- model/User.java
+++ model/User.java
@@ -1,5 +1,19 @@
 package model;
 
 public class User {
-    
+    private String name;
+    private String accountNumber;
+
+    public User(String name, String accountNumber) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
 }
