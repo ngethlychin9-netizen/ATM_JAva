aaab2764d03ad3ccdd08aaaa1a76ae50a4a85742
Maly pung <malylyyuya@gmail.com>
2026-09-22 22:43:22 +0700

    Card.java, User.java, and SavingsAccount.java are added with some errors. Please check and fix it

---
+++ model/SavingsAccount.java
@@ -0,0 +1,24 @@
+package model;
+
+import exception.InsufficientFundsException;
+
+public class SavingsAccount extends Account {
+
+    public SavingsAccount(String accountNumber,
+                          String pin,
+                          double balance) {
+        super(accountNumber, pin, balance);
+    }
+
+    @Override
+    public void withdraw(double amount)
+            throws InsufficientFundsException {
+
+        if(amount > getBalance()) {
+            throw new InsufficientFundsException(
+                    "Insufficient Balance");
+        }
+
+        deductBalance(amount);
+    }
+}
